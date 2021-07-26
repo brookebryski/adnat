973e92192e562248872b3c6bc761e840126a02a2
github username <github email address>
2021-07-26 11:44:00 -0500

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,43 @@
 class SessionsController < ApplicationController
+
+    def welcome
+    end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+    
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          flash[:error] = "Sorry, your username or password was incorrect"
+          redirect_to '/login'
+        end
+      end
+
+    #def fbcreate
+      #  @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      #    u.username = auth['info']['name']
+       #   u.email = auth['info']['email']
+      #    u.password = auth['uid']   # Secure Random Hex
+      #  end
+
+     #   session[:user_id] = @user.id
+
+     #   redirect_to '/organisations'
+ # end
+
+
+    private 
+
+    #def auth
+      #  request.env['omniauth.auth']
+   # end
 end
